17f15a467647d17c4fedf6a1f2d888cc83ead26a
dynamis <dynamis@firehacks.org>
2022-02-24 06:33:47 +0900

    update hello-aws-js-sdk

--- hello-aws-js-sdk/index.js
+++ hello-aws-js-sdk/index.js
@@ -1,5 +1,7 @@
 // S3 操作に使用するコマンドを AWS JS SDK からインポート
 import { ListBucketsCommand } from "@aws-sdk/client-s3";
+// HTTP サーバ
+import http from "http";
 // S3 操作用のヘルパークライアントオブジェクト
 import { s3Client } from "./libs/s3Client.js";
 
@@ -12,3 +14,11 @@ const run = async () => {
   }
 };
 run();
+
+http
+  .createServer(async (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.write(`Running Node ${process.version}`);
+    res.end(" 実行結果はターミナル参照");
+  })
+  .listen(8080);
