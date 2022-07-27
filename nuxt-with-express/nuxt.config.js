97d50344f2d1c87156b58e2b4f53c64d6029c8a2
dynamis <dynamis@firehacks.org>
2022-07-27 12:47:17 +0900

    update nuxt-with-express for latest nuxt3

--- nuxt-with-express/nuxt.config.js
+++ nuxt-with-express/nuxt.config.js
@@ -1,6 +1,6 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  serverMiddleware: [{ path: "/express", handler: "~/express/index.js" }],
+  serverMiddleware: [{ path: "/api", handler: "~/express/index.js" }],
 });
