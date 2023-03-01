0e920d216d2b3de30858cd0bd5a9b4ec1f138f27
dynamis <dynamis@firehacks.org>
2023-03-01 09:45:18 +0900

    http response json file, not only console.log

--- hello-aws-js-sdk/index.js
+++ hello-aws-js-sdk/index.js
@@ -5,20 +5,20 @@ import http from "http";
 // S3 操作用のヘルパークライアントオブジェクト
 import { s3Client } from "./libs/s3Client.js";
 
-const run = async () => {
+async function listBuckets() {
   try {
     const data = await s3Client.send(new ListBucketsCommand({}));
     console.log("バケットリスト: ", data.Buckets);
+    return data.Buckets;
   } catch (err) {
     console.log("エラー: ", err);
+    return "error!";
   }
-};
-run();
+}
 
 http
   .createServer(async (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-    res.write(`Running Node ${process.version}`);
-    res.end(" 実行結果はターミナル参照");
+    res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+    res.end(JSON.stringify(await listBuckets()));
   })
   .listen(8080);
