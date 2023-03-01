30674539804b89f64dfd3b10146099c7bc916334
dynamis <dynamis@firehacks.org>
2023-03-02 05:53:49 +0900

    add server handler example with catch-all route

--- nuxt-with-express/nuxt.config.js
+++ nuxt-with-express/nuxt.config.js
@@ -2,5 +2,5 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config#serverhandlers
 export default defineNuxtConfig({
-  serverHandlers: [{ route: "/api/**", handler: "~/express/index.js" }],
+  serverHandlers: [{ route: "/anotherapi/**", handler: "~/express/index.js" }],
 });
--- nuxt-with-express/server/api/[...].js
+++ nuxt-with-express/server/api/[...].js
@@ -7,16 +7,14 @@ const router = express.Router();
 app.use(bodyParser.json());
 
 router.get("/hello", (req, res) => {
-  res.send(
-    "Hello Express from server Handler assigned to this route with serverHandlers config"
-  );
+  res.send("Hello Express from catch-all route server Handler");
 });
 
 router.all("/json", (req, res) => {
   res.json({ data: "some data as JSON" });
 });
 
-app.use("/anotherapi", router);
+app.use("/api", router);
 
 // convert node middleware to h3 handlar
 // ref: https://github.com/nuxt/framework/releases/tag/v3.0.0-rc.12
